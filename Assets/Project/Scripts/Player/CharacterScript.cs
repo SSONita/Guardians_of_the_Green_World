59acf3b18a96b16156b42b1f4866cd27b4f4766e
SSONita <sarsovannita4@gmail.com>
2025-12-09 03:07:31 +0700

    finish setting up scence

---
+++ Assets/Project/Scripts/Player/CharacterScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private float verticalRotation = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Camera look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -60f, 60f);
+        cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+}
+
+
